c484499f8dc79ebeefa0d98e335ea0ce05dc44fd
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-04-29 12:25:27 -0500

    Map Mapking

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'location_services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 /* Currently used in location_services.dart:
   * import 'package:firebase_auth/firebase_auth.dart';
   * import 'package:cloud_firestore/cloud_firestore.dart';
@@ -47,16 +49,44 @@ class ScaffoldExample extends StatefulWidget {
 }
 
 class _ScaffoldExampleState extends State<ScaffoldExample> {
-  int _count = 0;
-
   @override
   Widget build(BuildContext context) {
+    // Options for our map:
+    const MapOptions mapOptions = MapOptions(
+      initialCenter: LatLng(48.7250079,-122.5128632),
+      initialZoom: 16.0,
+    );
+    // Tile layer represents the map we're using.
+    TileLayer tileLayer = TileLayer(
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'com.example.app',
+    );
+    // Marker layer represents the location we're at.
+    const MarkerLayer markerLayer = MarkerLayer(
+      markers: [
+        Marker(
+          width: 80.0,
+          height: 80.0,
+          point: LatLng(48.7216016,-122.5094043),
+          child: Icon(Icons.location_on, size: 50.0, color: Colors.red),
+        ),
+      ],
+    );
+    // Map layer:
+    FlutterMap flutterMap = FlutterMap(
+          options: mapOptions,
+          children: [
+            tileLayer,
+            markerLayer,
+          ]
+        );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sample Code'),
       ),
       body: Center(
-        child: Text('You have pressed the button $_count times.'),
+        child: flutterMap
       ),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
@@ -64,7 +94,7 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => setState(() {
-          _count++;
+          //! Start Trip!
         }),
         tooltip: 'Start Trip',
         label: const Text('Start Trip'),
