b034451502e49340005a9b725d089d8cb5d4f0fe
Lev Shuster <shusterl@carleton.edu>
2024-05-01 18:02:06 -0500

    standardised padding and removed bottom bar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -17,187 +17,195 @@ import 'package:latlong2/latlong.dart';
 	* Perhaps we can do this in the background, but for now we'll just do it here.
 */
 
+const double defaultPadding = 16;
 void main() async {
-	WidgetsFlutterBinding.ensureInitialized();
-	await Firebase.initializeApp(
-		options: DefaultFirebaseOptions.currentPlatform,
-	);
-	runApp(const CommunityBoatingTrackerApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const CommunityBoatingTrackerApp());
 }
 
 class CommunityBoatingTrackerApp extends StatelessWidget {
-	const CommunityBoatingTrackerApp({super.key});
+  const CommunityBoatingTrackerApp({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-		return const MaterialApp(
-			home: CommunityBoatingTracker(),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CommunityBoatingTracker(),
+    );
+  }
 }
 
 class CommunityBoatingTracker extends StatefulWidget {
-	const CommunityBoatingTracker({super.key});
+  const CommunityBoatingTracker({super.key});
 
-	@override
-	State<CommunityBoatingTracker> createState() =>
-			_CommunityBoatingTrackerState();
+  @override
+  State<CommunityBoatingTracker> createState() =>
+      _CommunityBoatingTrackerState();
 }
 
 class _CommunityBoatingTrackerState extends State<CommunityBoatingTracker> {
-	@override
-	Widget build(BuildContext context) {
-		// Options for our map:
-		const MapOptions mapOptions = MapOptions(
-			initialCenter: LatLng(48.7250079, -122.5128632),
-			initialZoom: 16.0,
-		);
-		// Tile layer represents the map we're using.
-		TileLayer tileLayer = TileLayer(
-			urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-			userAgentPackageName: 'com.example.app',
-		);
-		// Marker layer: [<home>, <current location>]
-		List<Marker> markers = [
-			const Marker(
-				width: 80.0,
-				height: 80.0,
-				point: LatLng(48.7216016, -122.5094043),
-				child: Icon(Icons.location_on, size: 50.0, color: Colors.red),
-			),
-			const Marker(
-				width: 80.0,
-				height: 80.0,
-				point: LatLng(48.7216016, -122.5094043),
-				child: Icon(Icons.location_on, size: 50.0, color: Colors.red),
-			)
-		];
+  @override
+  Widget build(BuildContext context) {
+    // Options for our map:
+    const MapOptions mapOptions = MapOptions(
+      initialCenter: LatLng(48.7250079, -122.5128632),
+      initialZoom: 16.0,
+    );
+    // Tile layer represents the map we're using.
+    TileLayer tileLayer = TileLayer(
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'com.example.app',
+    );
+    // Marker layer: [<home>, <current location>]
+    List<Marker> markers = [
+      const Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(48.7216016, -122.5094043),
+        child: Icon(Icons.location_on, size: 50.0, color: Colors.red),
+      ),
+      const Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(48.7216016, -122.5094043),
+        child: Icon(Icons.location_on, size: 50.0, color: Colors.red),
+      )
+    ];
 
-		// Marker layer represents the location we're at.
-		MarkerLayer markerLayer = MarkerLayer(
-			markers: markers,
-		);
-		// Map layer:
-		FlutterMap flutterMap = FlutterMap(
-			options: mapOptions,
-			mapController: MapController(),
-			children: [tileLayer, markerLayer],
-		);
+    // Marker layer represents the location we're at.
+    MarkerLayer markerLayer = MarkerLayer(
+      markers: markers,
+    );
+    // Map layer:
+    FlutterMap flutterMap = FlutterMap(
+      options: mapOptions,
+      mapController: MapController(),
+      children: [tileLayer, markerLayer],
+    );
 
-		TextField hullNumberField = const TextField(
-			decoration: InputDecoration(
-				border: OutlineInputBorder(),
-				hintText: 'Hull Number',
-			),
-		);
-		TextField emailField = const TextField(
-			decoration: InputDecoration(
-				border: OutlineInputBorder(),
-				hintText: 'Email Address',
-			),
-			enableSuggestions: true,
-			autocorrect: true,
-			autofillHints: [AutofillHints.email],
-		);
+    TextField hullNumberField = const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Hull Number',
+      ),
+    );
+    TextField emailField = const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Email Address',
+      ),
+      enableSuggestions: true,
+      autocorrect: true,
+      autofillHints: [AutofillHints.email],
+    );
 
-		DropdownButtonFormField activityTypeField = DropdownButtonFormField<String>(
-			decoration: const InputDecoration(
-				border: OutlineInputBorder(),
-				hintText: 'Activity Type',
-			),
-			items: const [
-				DropdownMenuItem(
-					value: 'lesson',
-					child: Text('Lesson'),
-				),
-				DropdownMenuItem(
-					value: 'rental',
-					child: Text('Rental'),
-				),
-				DropdownMenuItem(
-					value: 'admin',
-					child: Text('Admin'),
-				),
-			],
-			onChanged: (value) {
-				// Handle the selected value here
-			},
-		);
+    DropdownButtonFormField activityTypeField = DropdownButtonFormField<String>(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Activity Type',
+      ),
+      items: const [
+        DropdownMenuItem(
+          value: 'lesson',
+          child: Text('Lesson'),
+        ),
+        DropdownMenuItem(
+          value: 'rental',
+          child: Text('Rental'),
+        ),
+        DropdownMenuItem(
+          value: 'admin',
+          child: Text('Admin'),
+        ),
+      ],
+      onChanged: (value) {
+        // Handle the selected value here
+      },
+    );
 
-		Column bodyColumn = Column(
-			children: <Widget>[
-				Row(
-					children: <Widget>[
-						Expanded(
-							child: hullNumberField,
-						),
-						const SizedBox(width: 10),
-						Expanded(
-							child: emailField,
-						),
-						const SizedBox(width: 10),
-						Expanded(child: activityTypeField),
-					],
-				),
-				const SizedBox(height: 10),
-				Expanded(child: flutterMap),
-			],
-		);
+    // Add padding to the column
+    Padding bodyColumn = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: hullNumberField,
+              ),
+              const SizedBox(width: defaultPadding),
+              Expanded(
+                child: emailField,
+              ),
+              const SizedBox(width: defaultPadding),
+              Expanded(child: activityTypeField),
+            ],
+          ),
+          const SizedBox(height: defaultPadding),
+          Expanded(child: flutterMap),
+        ],
+      ),
+    );
 
-		// Location services for tracking our location:
-		LocationService locationService = LocationService();
-		// bool tracking = false;
-		ValueNotifier<bool> tracking = ValueNotifier<bool>(false);
+    // Location services for tracking our location:
+    LocationService locationService = LocationService();
+    // bool tracking = false;
+    ValueNotifier<bool> tracking = ValueNotifier<bool>(false);
 
-		// Text tripButtonText = tracking ? Text('End Trip') : Text('Start Trip');
-		// Timer:
-		Timer locationTracker =
-				Timer.periodic(const Duration(seconds: 30), (timer) async {
-			// Get our location:
-			final locationData = await locationService.getCurrentLocation();
-			// center ourselves on the map:
-			LatLng point = LatLng(locationData.latitude!, locationData.longitude!);
-			flutterMap.mapController?.move(
-					point,
-					flutterMap.mapController?.camera.zoom ??
-							16.0); //! Do we want to force a cameramove?
-			// Add our current location to the map:
-			markers.removeLast();
-			markers.add(Marker(
-					point: point,
-					child: const Icon(Icons.location_on, size: 50.0, color: Colors.red)));
-			// Add our current location to the path and send to the server if tracking:
-			if (tracking.value) {
-				// Send our location to the server:
-				locationService.sendLocationToServer(
-						'TestID', LatLng(locationData.latitude!, locationData.longitude!));
-			}
-		});
+    // Text tripButtonText = tracking ? Text('End Trip') : Text('Start Trip');
+    // Timer:
+    Timer locationTracker =
+        Timer.periodic(const Duration(seconds: 30), (timer) async {
+      // Get our location:
+      final locationData = await locationService.getCurrentLocation();
+      // center ourselves on the map:
+      LatLng point = LatLng(locationData.latitude!, locationData.longitude!);
+      flutterMap.mapController?.move(
+          point,
+          flutterMap.mapController?.camera.zoom ??
+              16.0); //! Do we want to force a cameramove?
+      // Add our current location to the map:
+      markers.removeLast();
+      markers.add(Marker(
+          point: point,
+          child: const Icon(Icons.location_on, size: 50.0, color: Colors.red)));
+      // Add our current location to the path and send to the server if tracking:
+      if (tracking.value) {
+        // Send our location to the server:
+        locationService.sendLocationToServer(
+            'TestID', LatLng(locationData.latitude!, locationData.longitude!));
+      }
+    });
 
-		return Scaffold(
-			appBar: AppBar(
-				title: const Text('Community Boating Center Rental Tracker'),
-			),
-			body: bodyColumn,
-			bottomNavigationBar: BottomAppBar(
-				shape: const CircularNotchedRectangle(),
-				notchMargin: -100,
-				child: Container(height: 50.0),
-			),
-			floatingActionButton: FloatingActionButton.extended(
-				onPressed: () => tracking.value = !tracking.value,
-				tooltip:
-						'Before Leaving the Dock, start your trip to help us manage our fleet.',
-				// label: tracking.value ? const Text('End Trip') : const Text('Start Trip'),
-				label: ValueListenableBuilder(
-					valueListenable: tracking,
-					builder: (context, value, child) {
-						return value ? const Text('End Trip') : const Text('Start Trip');
-					},
-				),
-				icon: const Icon(Icons.navigation),
-			),
-			floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-		);
-	}
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Community Boating Center Rental Tracker'),
+      ),
+      body: bodyColumn,
+      // bottomNavigationBar: BottomAppBar(
+      //   shape: const CircularNotchedRectangle(),
+      //   notchMargin: -100,
+      //   child: Container(height: 20.0),
+      // ),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(bottom: defaultPadding),
+        child: FloatingActionButton.extended(
+          onPressed: () => tracking.value = !tracking.value,
+          tooltip:
+              'Before Leaving the Dock, start your trip to help us manage our fleet.',
+          // label: tracking.value ? const Text('End Trip') : const Text('Start Trip'),
+          label: ValueListenableBuilder(
+            valueListenable: tracking,
+            builder: (context, value, child) {
+              return value ? const Text('End Trip') : const Text('Start Trip');
+            },
+          ),
+          icon: const Icon(Icons.navigation),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
 }
