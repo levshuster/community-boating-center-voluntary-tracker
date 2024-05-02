6f7eb991eda7092afe82d8d227f2687158583452
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-05-01 22:28:00 -0500

    beginning of admin panel

--- app/lib/main_admin.dart
+++ app/lib/main_admin.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'location_services.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -83,42 +84,17 @@ class _CommunityBoatingTrackerState extends State<CommunityBoatingTracker> {
       urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
       userAgentPackageName: 'com.example.app',
     );
-    // Polyline layer
-    List<LatLng> points = [];
-    PolylineLayer polylineLayer = PolylineLayer(
-      polylines: [
-        Polyline(
-          points: points,
-          strokeWidth: 4.0,
-          color: Colors.red,
-        ),
-      ],
-    );
-    // Marker layer: [<home>, <current location>]
-    List<Marker> markers = [
-      const Marker(
-        width: 80.0,
-        height: 80.0,
-        point: LatLng(48.7216016, -122.5094043),
-        child: Icon(Icons.location_on, size: 50.0, color: Colors.green),
-      ),
-      const Marker(
-        width: 80.0,
-        height: 80.0,
-        point: LatLng(48.7216016, -122.5094043),
-        child: Icon(Icons.location_on, size: 50.0, color: Colors.green),
-      )
-    ];
-
-    // Marker layer represents the location we're at.
+    // Marker Layer:
+    List<Marker> markers = [];
     MarkerLayer markerLayer = MarkerLayer(
       markers: markers,
     );
+
     // Map layer:
     FlutterMap flutterMap = FlutterMap(
       options: mapOptions,
       mapController: MapController(),
-      children: [tileLayer, markerLayer, polylineLayer],
+      children: [tileLayer, markerLayer],
     );
 
     ValueNotifier<bool> tracking = ValueNotifier<bool>(false);
@@ -243,36 +219,46 @@ class _CommunityBoatingTrackerState extends State<CommunityBoatingTracker> {
       ),
     );
 
-    // Location services for tracking our location:
-    LocationService locationService = LocationService();
-
-    // Timer:
-    Timer locationTracker =
-        Timer.periodic(const Duration(seconds: 10), (timer) async {
-      // Get our location:
-      final locationData = await locationService.getCurrentLocation();
-      // center ourselves on the map:
-      LatLng point = LatLng(locationData.latitude!, locationData.longitude!);
-      flutterMap.mapController?.move(
-          point,
-          flutterMap.mapController?.camera.zoom ?? 16.0); //! Do we want to force a cameramove?
+    //! Timer:
+    Timer locationTracker = Timer.periodic(const Duration(seconds: 10), (timer) async {
       // Add our current location to the map:
-      markers.removeLast();
-      markers.add(Marker(
-          point: point,
-          child: const Icon(Icons.directions_boat_filled_rounded, 
-                            size: 50.0, 
-                            color: Colors.red),
+      markers.removeWhere((element) => true);
+
+      // Pull all location data in the last 20 minutes from the firebase server:
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      QuerySnapshot querySnapshot = await firestore
+        .collection('Location')
+        .where('timestamp', isGreaterThan: Timestamp
+          .now()
+          .toDate()
+          .subtract(const Duration(minutes: 20)))
+          .orderBy('timestamp', descending: true) // TODO: WHY DON'T YOU WORK?
+        .get();
+      
+      // From this query snapshot, get each unique most recent marker:
+      List<String> temp_ids = [];
+      querySnapshot.docs.forEach((element) {
+        LatLng point = LatLng(element['location'].latitude, element['location'].longitude);
+        String id = element["user_id"];
+        // String id = element['user_id'] + element['boat_id']; // TODO: This might break lmao
+
+        // Check if we have a marker for this user already:
+        if (!temp_ids.contains(id)) {
+          markers.add(Marker(
+            width: 80.0,
+            height: 80.0,
+            point: point,
+            child: const Icon(Icons.directions_boat_filled_rounded, 
+                              size: 50.0, 
+                              color: Colors.red), // TODO: Change color based on activity/role
           ));
-      // Add our current location to the path and send to the server if tracking:
-      if (tracking.value) {
-        // Add to our trip:
-        points.add(point);
-        // Send our location to the server:
-        locationService.sendLocationToServer(
-            'TestID', 
-            LatLng(locationData.latitude!, locationData.longitude!));
-      }
+          temp_ids.add(id);
+        }
+        
+
+      });
+
+
     });
 
     return Scaffold(
@@ -280,11 +266,6 @@ class _CommunityBoatingTrackerState extends State<CommunityBoatingTracker> {
         title: const Text('Community Boating Center Rental Tracker'),
       ),
       body: bodyColumn,
-      // bottomNavigationBar: BottomAppBar(
-      //   shape: const CircularNotchedRectangle(),
-      //   notchMargin: -100,
-      //   child: Container(height: 20.0),
-      // ),
       floatingActionButton: Container(
         margin: const EdgeInsets.only(bottom: defaultPadding),
         child: FloatingActionButton.extended(
